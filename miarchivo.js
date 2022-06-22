186157d2bc9dae7cdb0c52d08077289ccaefb41b
piperno <juan.piperno@gmail.com>
2022-06-22 19:55:33 -0300

    v1

---
+++ miarchivo.js
@@ -0,0 +1,66 @@
+function calcule (precio, cant, provincia) {
+    let total = Math.trunc(precio / cant);
+    document.getElementById("section").style.display = 'inline-block';
+    switch(provincia){
+        case "ushuaia":
+            document.getElementById("provincia").innerHTML = 'Ushuaia';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        case "bariloche":
+            document.getElementById("provincia").innerHTML = 'Bariloche';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        case "mendoza":
+            document.getElementById("provincia").innerHTML = 'Mendoza';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        default:
+            break;
+    }
+}
+
+
+// Constructor
+
+function capturar (){
+    function Ciudad (nombre, epoca, gastronomia){
+        this.nombre = nombre;
+        this.epoca = epoca;
+        this.gastronomia = gastronomia;
+    }
+    var nombreCapturar = document.getElementById("nombre").value;
+    var epocaCapturar = document.getElementById("epoca").value;
+    var gastronomiaCapturar = document.getElementById("gastronomia").value;
+
+    nuevoSujeto = new Ciudad (nombreCapturar, epocaCapturar, gastronomiaCapturar);
+    console.log(nuevoSujeto);
+    agregar ();
+};
+    var baseDatos= [];
+    function agregar() {
+        baseDatos.push(nuevoSujeto)
+        document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>'+nuevoSujeto.epoca+'</td><td>'+nuevoSujeto.gastronomia+'</td></tbody>';
+};
+
+
+// DOM para Contador
+
+const btnAgregar = document.querySelector('.btn-info');
+const btnDisminuir = document.querySelector ('.btn-danger');
+const span = document.getElementById('span');
+let contador = 0;
+
+btnAgregar.addEventListener('click', () => {
+    console.log('Diste click efectivamente');
+    contador++;
+    span.textContent = contador
+});
+
+btnDisminuir.addEventListener('click', () => {
+    console.log('Diste click efectivamente');
+    contador--;
+    span.textContent = contador
+});
+
+
+// Evento 
